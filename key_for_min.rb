36e2f68b8ab0b9f91f65f930c4adb3ea02cda6c6
Mary Boyd <mary.boyd314@gmail.com>
2016-07-11 22:40:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,21 @@
 
 def key_for_min_value(name_hash)
 
+  min_value = 10000
+  min_key = 0
+
+  if name_hash.empty?
+    return nil
+  end
+
+  name_a = name_hash.to_a
+
+  name_a.each do |i|
+    if i[1] < min_value
+      min_value = i[1]
+      min_key = i[0]
+    end
+
+end
+return min_key
 end
